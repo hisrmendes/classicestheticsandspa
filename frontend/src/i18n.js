5022c68ca7cab5ab7092d4adcd930e459e26dac3
hisrmendes <hisrmendes@gmail.com>
2025-11-16 13:35:53 +0100

    Update translation files and language settings

--- frontend/src/i18n.js
+++ frontend/src/i18n.js
@@ -1,13 +1,24 @@
 import i18n from 'i18next'
 import { initReactI18next } from 'react-i18next'
-import es from './locales/es.json'
-import pt from './locales/pt.json'
-import en from './locales/en.json'
 
-i18n.use(initReactI18next).init({
-  resources:{es:{translation:es},pt:{translation:pt},en:{translation:en}},
-  lng:'es',
-  fallbackLng:'es',
-  interpolation:{escapeValue:false}
-})
+// importa os arquivos de tradução
+import es from './locales/es.json'
+import pt from './locales/pt-br.json'
+import en from './locales/en-us.json'
+
+i18n
+  .use(initReactI18next)
+  .init({
+    resources: {
+  es: { translation: es },
+  'pt-BR': { translation: pt },
+  'en-US': { translation: en },
+},
+    lng: 'es', // idioma padrão (Espanhol)
+    fallbackLng: 'es',
+    interpolation: {
+      escapeValue: false,
+    },
+  })
+
 export default i18n
